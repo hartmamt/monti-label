e4c7fa6683b48364c95f0ef70950326a54ef9c50
Matthew Hartman <>
2017-06-22 13:13:40 -0400

    enable dev tools for now

--- src/index.js
+++ src/index.js
@@ -41,10 +41,10 @@ const createWindow = async () => {
   // });
 
   // Open the DevTools.
-  if (isDevMode) {
-    await installExtension(REACT_DEVELOPER_TOOLS);
-    mainWindow.webContents.openDevTools();
-  }
+  // if (isDevMode) {
+  await installExtension(REACT_DEVELOPER_TOOLS);
+  mainWindow.webContents.openDevTools();
+  //  }
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
